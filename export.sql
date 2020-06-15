e512a5f022ac69d599f086e0b707dbbed11b249c
DarthJake <j.e.ellsworth.2000@gmail.com>
2020-06-15 09:19:35 -0700

    Updated MYSQL dump file.

--- export.sql
+++ export.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
 --
 -- Host: localhost    Database: mydb
 -- ------------------------------------------------------
--- Server version	8.0.16
+-- Server version	8.0.18
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8mb4 ;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `carts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `carts` (
   `cart_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE `carts` (
   `date` date NOT NULL DEFAULT (curdate()),
   `expired` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`cart_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +39,7 @@ CREATE TABLE `carts` (
 
 LOCK TABLES `carts` WRITE;
 /*!40000 ALTER TABLE `carts` DISABLE KEYS */;
+INSERT INTO `carts` VALUES (5,5,2,2,'2019-10-11',0),(24,10,1,1,'2020-06-14',0);
 /*!40000 ALTER TABLE `carts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -48,7 +49,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `products`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `products` (
   `product_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
@@ -57,7 +58,7 @@ CREATE TABLE `products` (
   `description` text NOT NULL,
   `origin` varchar(20) DEFAULT 'America',
   PRIMARY KEY (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +67,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'Brush',15.98,'Wet Brush','A pretty good brush for getting out nasty tangles.','America'),(2,'Gaming Mouse',5.21,'GudStufz','A cheap but working gaming mouse.','China');
+INSERT INTO `products` VALUES (1,'Brush',15.98,'Wet Brush','A pretty good brush for getting out nasty tangles.','America'),(2,'Gaming Mouse',5.21,'GudStufz','A cheap but working gaming mouse.','America'),(3,'Big Brush',19.99,'Wet Brush','For BIG tangles.','America');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,14 +77,16 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(20) NOT NULL,
   `last_name` varchar(20) NOT NULL,
   `username` varchar(20) NOT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `is_vender` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,7 +95,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'John','Smith','DoctorWho'),(2,'John','Doe','jdoe'),(3,'Bob','Bobber','bobbbobber'),(4,'Steve','MC','MCIsLife'),(5,'Steve','MC','MCIsLife'),(6,'Bill','Dave','BD'),(7,'Bill','Dave','BD'),(8,'Dave','Dave','DaveME'),(9,'Jane','Doe','JaDoe'),(10,'Doe','Doe','DoeDoe'),(11,'Steve','Stever','StevesStever');
+INSERT INTO `users` VALUES (1,'B','B','B','B',0),(2,'Jake','Ellsworth','J','1234',0),(3,'Bob','Bobber','BobBobber','246',0),(4,'S','S','S','pass',0),(5,'Nathan','W','Nat','password',0),(6,'Jacob','Smellsworth','Usmell45','ureallysmell',0),(7,'Wyatt','G','WG','pass',0),(8,'TestNonVend','Test','NotVen','pass',0),(9,'Ven','Der','V','V',1),(10,'A','A','A','A',0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -105,4 +108,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-10-09 15:12:07
+-- Dump completed on 2020-06-14 17:59:52
